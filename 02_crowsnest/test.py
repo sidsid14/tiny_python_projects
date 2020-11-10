e405a459227ba94190069631dc3d3159a301cecb
sudhanshu <sidsid14@gmail.com>
2020-11-10 22:40:08 +0530

    Chapter 2 completed

--- 02_crowsnest/test.py
+++ 02_crowsnest/test.py
@@ -47,7 +47,7 @@ def test_consonant_upper():
 
     for word in consonant_words:
         out = getoutput(f'{prg} {word.title()}')
-        assert out.strip() == template.format('a', word.title())
+        assert out.strip() == template.format('A', word.title())
 
 
 # --------------------------------------------------
@@ -65,4 +65,6 @@ def test_vowel_upper():
 
     for word in vowel_words:
         out = getoutput(f'{prg} {word.upper()}')
-        assert out.strip() == template.format('an', word.upper())
+        assert out.strip() == template.format('An', word.upper())
+
+# --------------------------------------------------
